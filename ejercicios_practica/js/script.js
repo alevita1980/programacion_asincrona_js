504a0219a1b0b5fc531888044bb62f2382c6e63b
Ale <103283338+alevita1980@users.noreply.github.com>
2022-08-09 16:31:50 -0300

    Add files via upload

--- ejercicios_practica/js/script.js
+++ ejercicios_practica/js/script.js
@@ -17,36 +17,53 @@ y leer los datos ingresados en "personaje".
 - Ejemplo de la URL con la variable personaje:
 - * `https://rickandmortyapi.com/api/character/?name=${personaje}`
 - Capture los datos del personaje en un JSON e imprimalos en consola.
+
+## 2 - Enunciado
+
+- Capture la información del personaje y preséntela en el HTML, puede
+  presentar la información como los ejemplos que se muestran en la página
+  de la API:
+
+![personaje_biblesuarus](personaje_biblesaurus.jpg)
+
+- Puede agregar o no estilos al HTML, o presentar la información de forma cruda (queda a su criterio y creatividad).
 */
 
 const btn = document.getElementById("btnConsultar");
 btn.onclick = () =>{
-  const personaje = document.getElementById('personaje').value;
-   let url = "https://rickandmortyapi.com/api/character/?name=Biblesaurus"
+  const personaje = document.querySelector("#personaje").value;
+   let url = `https://rickandmortyapi.com/api/character/?name=${personaje}`;
    fetch(url)
-   .then(Response =>Response.json())
+   .then(Resp =>Resp.json())
    .then(data =>{
-    console.log(data)
+    console.log(data.results[0])
     console.log(personaje)
-console.log(url)
-    let contenedor=""
+    console.log(url)
     
-      contenedor+=
-    ` <p> ${data.id}</p>
-      <p> ${data.name}</p>
-      <img src= "${data.url}" alt="">
-      <img src= "${data.image}" alt="">
-      <p> ${data.species}</p>
-      <p> ${data.status}</p>
-
+    data.results.forEach(Element=> {
+    const contenedor = document.createElement("div")
+    contenedor.style.background="grey"
+    contenedor.style.color= "white"
+    
+    contenedor.innerHTML = `
+      <table>
+      <img src= "${Element.url}" alt="">
+      <img src= "${Element.image}" align="left" width="348px">
+      <h1> ${Element.name}</h1>
+      <h2><li> ${Element.status} - ${Element.species}</li></h2>
+      <p> Last Known location </p>
+      <h2> ${Element.location.name}</h2>
+      <p> First seen in: </p>
+      <h2> Never Ricking Morty</h2>
+      </table>
       `
-     document.getElementById("personaje").innerHTML=contenedor
+     document.body.appendChild(contenedor);
 
   });
 
-}
+})
 
-  
+} 
 
 
     
